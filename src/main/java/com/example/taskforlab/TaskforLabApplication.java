c908f65b5c095bff6bbd72a6389df5d969501244
Sergey26-zh <ivahnenkose.2003@gmail.com>
2024-01-18 11:57:46 +0700

    create project

---
+++ src/main/java/com/example/taskforlab/TaskforLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskforlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskforLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskforLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskforlab/TaskforLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskforlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskforLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
